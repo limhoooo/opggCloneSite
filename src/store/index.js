18f9cdf24eec2ff818b721741cf169ac1c9d85a0
lim ho <dlagh123@gmail.com>
2023-04-22 10:18:10 +0900

    lint

--- src/store/index.js
+++ src/store/index.js
@@ -1,84 +1,86 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-import { getSummoner, getMostInfo, getMatchList, getMatchDetail } from "../api/opggApi";
+import Vue from "vue";
+import Vuex from "vuex";
+import {
+  getSummoner,
+  getMostInfo,
+  getMatchList,
+  getMatchDetail,
+} from "../api/opggApi";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {
-        userInfo: [],
-        ladderRank: [],
-        previousTiers: [],
-        leagues: [],
-        mostInfoChampions: [],
-        mostInfoRecentWinRate: [],
-        champions: [],
-        games: [],
-        gamesCapyData: [],
-        positions: [],
-        summary: [],
-        testtt: [],
+  state: {
+    userInfo: [],
+    ladderRank: [],
+    previousTiers: [],
+    leagues: [],
+    mostInfoChampions: [],
+    mostInfoRecentWinRate: [],
+    champions: [],
+    games: [],
+    gamesCapyData: [],
+    positions: [],
+    summary: [],
+    testtt: [],
+  },
+  getters: {},
+  mutations: {
+    setUserInfo(state, data) {
+      state.userInfo = data.summoner;
+      state.ladderRank = data.summoner.ladderRank;
+      state.previousTiers = data.summoner.previousTiers;
+      state.leagues = data.summoner.leagues;
     },
-    getters: {
-
+    setMostInfo(state, data) {
+      state.mostInfoChampions = data.champions.sort(
+        (a, b) => b.games - a.games
+      );
+      state.mostInfoRecentWinRate = data.recentWinRate;
     },
-    mutations: {
-        setUserInfo(state, data) {
-            state.userInfo = data.summoner;
-            state.ladderRank = data.summoner.ladderRank;
-            state.previousTiers = data.summoner.previousTiers;
-            state.leagues = data.summoner.leagues; RecentWinRate
-        },
-        setMostInfo(state, data) {
-            state.mostInfoChampions = data.champions.sort((a, b) => b.games - a.games);
-            state.mostInfoRecentWinRate = data.recentWinRate;
-        },
-        setMatchList(state, data) {
-            state.champions = data.champions;
-            state.games = data.games;
-            state.gamesCapyData = [...data.games];
-            state.positions = data.positions;
-            state.summary = data.summary;
-        },
-        setMatchDetail(state, data) {
-            state.testtt = data;
-        }
-
+    setMatchList(state, data) {
+      state.champions = data.champions;
+      state.games = data.games;
+      state.gamesCapyData = [...data.games];
+      state.positions = data.positions;
+      state.summary = data.summary;
     },
-    actions: {
-        async getSummonerFnc({ commit }, data) {
-            try {
-                const result = await getSummoner(data);
-                commit('setUserInfo', result.data)
-            } catch (error) {
-                console.log(error);
-            }
-        },
-        async getMostInfoFnc({ commit }, data) {
-            try {
-                const result = await getMostInfo(data);
-                commit('setMostInfo', result.data)
-            } catch (error) {
-                console.log(error);
-            }
-        },
-        async getMatchListFnc({ commit }, summonerName) {
-            try {
-                const result = await getMatchList(summonerName);
-                commit('setMatchList', result.data)
-            } catch (error) {
-                console.log(error);
-            }
-        },
-        async getMatchDetailFnc({ commit }, data) {
-            try {
-                const result = await getMatchDetail(data.summonerName, data.gameId);
-                return result.data;
-            } catch (error) {
-                console.log(error);
-            }
-        }
+    setMatchDetail(state, data) {
+      state.testtt = data;
     },
-
-
+  },
+  actions: {
+    async getSummonerFnc({ commit }, data) {
+      try {
+        const result = await getSummoner(data);
+        commit("setUserInfo", result.data);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async getMostInfoFnc({ commit }, data) {
+      try {
+        const result = await getMostInfo(data);
+        commit("setMostInfo", result.data);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async getMatchListFnc({ commit }, summonerName) {
+      try {
+        const result = await getMatchList(summonerName);
+        commit("setMatchList", result.data);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async getMatchDetailFnc({ commit }, data) {
+      try {
+        const result = await getMatchDetail(data.summonerName, data.gameId);
+        return result.data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
 });
